630812c2e12e4ea83b6755635135a2d2cd16c747
ahmed12363 <67515913+ahmed12363@users.noreply.github.com>
2021-08-04 01:49:20 +0300

    Update and rename WEB IP.py to webscanner.py

---
+++ webscanner.py
@@ -0,0 +1,27 @@
+from pyfiglet import Figlet
+custom_fig = Figlet(font='graffiti')
+print(custom_fig.renderText('Website Scanner'))
+print('By Yosif')
+print('')
+import os
+import socket
+import time
+USER = input("[+]Enter Website: ")
+time.sleep(0.3)
+print("")
+print("[+]Please Wait")
+print("")
+time.sleep(2)
+ip = socket.gethostbyname(USER)
+print(ip)
+from pyfiglet import Figlet
+custom_fig = Figlet(font='graffiti')
+print(custom_fig.renderText('by yosif'))
+
+
+
+
+
+
+
+
